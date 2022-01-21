83f15175fa8e709ca72f3faba12f35d6a0a28d06
galleu <59324021+galleu@users.noreply.github.com>
2022-01-21 08:20:22 +0200

    Removed unnecessary check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const options = request.data.options ? Object.fromEntries(new Map(request.data.options.map(o => {
     if (o.options) {
-        return [o.name, o.options ?  Object.fromEntries(new Map(o.options.map(v => [v.name, v.value]))) : null]
+        return [o.name, Object.fromEntries(new Map(o.options.map(v => [v.name, v.value])))]
     } else {
         return [o.name, o.value || null]
     }

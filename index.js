f109dcd12ee64ef96cf2f60e9d2147d110905348
galleu <59324021+galleu@users.noreply.github.com>
2022-01-21 08:17:15 +0200

    Edit to allow for null values

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const options = request.data.options ? Object.fromEntries(new Map(request.data.options.map(o => {
     if (o.options) {
-        return [o.name, Object.fromEntries(new Map(o.options.map(v => [v.name, v.value])))]
+        return [o.name, o.options ?  Object.fromEntries(new Map(o.options.map(v => [v.name, v.value]))) : null]
     } else {
-        return [o.name, o.value]
+        return [o.name, o.value || null]
     }
 }))) : {};
